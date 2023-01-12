bfbfa547d0f372518da7519d92c0141a75269782
coji <coji@techtalk.jp>
2023-01-12 19:52:45 +0900

    implement login

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -6,6 +6,8 @@ import type {
 
 export interface User {
   id: string
+  provider: string
+  providerUserId: string
   displayName?: string
   email?: string
   photoURL?: string
@@ -23,6 +25,8 @@ const converter: FirestoreDataConverter<User> = {
     const data = snapshot.data()
     return {
       id: snapshot.id,
+      provider: data.provider as string,
+      providerUserId: data.providerUserId as string,
       displayName: data.name as string,
       email: data.email as string,
       teamId: data.teamId as string,
@@ -30,24 +34,44 @@ const converter: FirestoreDataConverter<User> = {
   },
 }
 
-export const addUser = async (data: User) => {
+export const findUserByProviderUserId = async (
+  provider: string,
+  providerUserId: string,
+) => {
+  const users = await firestore
+    .collection('users')
+    .withConverter(converter)
+    .where('provider', '==', provider)
+    .where('providerUserId', '==', providerUserId)
+    .limit(1)
+    .get()
+
+  if (users.size === 0) {
+    return null
+  }
+  return users.docs[0].data()
+}
+
+export const addUser = async (data: Omit<User, 'id'>) => {
   const docRef = await firestore
     .collection('users')
     .withConverter(converter)
-    .add({ ...data })
-  return (await docRef.get()).data()
+    .add({ id: '', ...data })
+  return (await docRef.get()).data() ?? null
 }
 
 export const getUser = async (id: string) => {
   const user = await firestore.doc(`users/${id}`).withConverter(converter).get()
-  return user.data()
+  return user.data() ?? null
 }
 
-export const updateUser = async (user: User) =>
+export const updateUser = async (user: User) => {
   await firestore
     .doc(`users/${user.id}`)
     .withConverter(converter)
     .set(user, { merge: true })
+}
 
-export const deleteUser = async (user: User) =>
+export const deleteUser = async (user: User) => {
   await firestore.doc(`users/${user.id})`).delete()
+}
--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,18 +1,18 @@
 import { createCookieSessionStorage } from '@remix-run/node'
 import { Authenticator, type StrategyVerifyCallback } from 'remix-auth'
 import type { OAuth2StrategyVerifyParams } from 'remix-auth-oauth2'
-// import {
-//   GitHubStrategy,
-//   type GitHubProfile,
-//   type GitHubExtraParams,
-// } from 'remix-auth-github'
+import {
+  GitHubStrategy,
+  type GitHubProfile,
+  type GitHubExtraParams,
+} from 'remix-auth-github'
 import {
   SlackStrategy,
   type SlackExtraParams,
   type SlackProfile,
-} from '../features/oauth2-login/libs/SlackStrategy.server'
+} from './SlackStrategy.server'
 import invariant from 'tiny-invariant'
-import { getUser, addUser, type User } from '~/models/user.server'
+import { addUser, findUserByProviderUserId } from '~/models/user.server'
 
 invariant(process.env.BASE_URL, 'BASE_URL is required')
 invariant(process.env.SESSION_SECRET, 'SESSION_SECRET is required')
@@ -38,7 +38,28 @@ export const sessionStorage = createCookieSessionStorage({
 
 export const auth = new Authenticator<SessionUser>(sessionStorage)
 
-/*
+const githubVerify: StrategyVerifyCallback<
+  SessionUser,
+  OAuth2StrategyVerifyParams<GitHubProfile, GitHubExtraParams>
+> = async ({ profile }) => {
+  let user = await findUserByProviderUserId('github', profile.id)
+  if (!user) {
+    // 新規ユーザ
+    user = await addUser({
+      provider: 'github',
+      providerUserId: profile.id,
+      displayName: profile.displayName,
+      email: profile.emails?.[0].value,
+      photoURL: profile.photos?.[0].value,
+      teamId: '',
+    })
+  }
+  if (!user) {
+    throw new Error('User not found')
+  }
+  return { userId: user.id }
+}
+
 auth.use(
   new GitHubStrategy(
     {
@@ -49,21 +70,20 @@ auth.use(
         process.env.BASE_URL,
       ).toString(),
     },
-    // eslint-disable-next-line @typescript-eslint/require-await
-    async ({ profile, accessToken, extraParams }) => {
-
-    },
+    githubVerify,
   ),
-) */
+)
 
 const slackVerify: StrategyVerifyCallback<
   SessionUser,
   OAuth2StrategyVerifyParams<SlackProfile, SlackExtraParams>
 > = async ({ profile, accessToken, extraParams }) => {
-  let user = await getUser(profile.id)
+  let user = await findUserByProviderUserId('slack', profile.id)
   if (!user) {
+    // 新規ユーザ
     user = await addUser({
-      id: profile.id,
+      provider: 'slack',
+      providerUserId: profile.id,
       displayName: profile.displayName,
       email: profile.emails?.[0].value,
       photoURL: profile.photos?.[0].value,
